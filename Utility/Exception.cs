4feb724fe8f0b4b2a77e3c42b8546de5ae0e0696
true_mogician <truemogician@gmail.com>
2025-12-18 23:05:04 -0800

    feat(Utility): add custom exceptions

---
+++ Utility/Exception.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Verse;
+
+namespace TrueMogician.RimWorld.Utility;
+
+public class CorruptedDataException : Exception {
+	public CorruptedDataException() { }
+
+	public CorruptedDataException(Type type) : base($"Corrupted save/settings data when loading type {type.AssemblyQualifiedName}") => Type = type;
+
+	public CorruptedDataException(string message, Type? type = null) : base(message) => Type = type;
+
+	public Type? Type { get; }
+}
+
+public class InvalidPawnException(string message, Pawn pawn) : Exception(message) {
+	public InvalidPawnException(Pawn pawn) : this($"Invalid pawn: {pawn.Name}", pawn) { }
+
+	public Pawn Pawn { get; } = pawn;
+}
