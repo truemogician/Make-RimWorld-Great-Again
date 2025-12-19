974f905db74fb3cfd3f763ae978ea09038e8bb71
true_mogician <truemogician@gmail.com>
2025-12-19 01:54:29 -0800

    feat(Rimsonable): grenade patch CE compatibility

--- Rimsonable/Rimsonable/Patches/CompShieldPatches.cs
+++ Rimsonable/Rimsonable/Patches/CompShieldPatches.cs
@@ -1,3 +1,4 @@
+using System;
 using HarmonyLib;
 using RimWorld;
 using Verse;
@@ -10,10 +11,15 @@ namespace TrueMogician.RimWorld.Rimsonable.Patches;
 public static class CompShieldPatches {
 	private static readonly ThingCategoryDef Grenades = DefDatabase<ThingCategoryDef>.GetNamed("Grenades");
 
+	private static readonly Type? CELaunchVerbType = Type.GetType("CombatExtended.Verb_LaunchProjectileCE, CombatExtended", false);
+
 	[HarmonyPatch(nameof(CompShield.CompAllowVerbCast))]
+	[HarmonyPriority(Priority.First)]
 	[HarmonyPrefix]
 	public static bool CompAllowVerbCast_Prefix(CompShield __instance, Verb verb, ref bool __result) {
-		if (!__instance.Props.blocksRangedWeapons || verb is not Verb_LaunchProjectile)
+		if (!__instance.Props.blocksRangedWeapons)
+			return true;
+		if (verb is not Verb_LaunchProjectile && (CELaunchVerbType is null || !CELaunchVerbType.IsInstanceOfType(verb)))
 			return true;
 		if (verb.EquipmentSource?.def.thingCategories is not { } categories || !categories.Contains(Grenades))
 			return true;
