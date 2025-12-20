f844de0702ec99e142b925e32bf39bcc76115345
true_mogician <truemogician@gmail.com>
2025-12-19 17:25:20 -0800

    feat(Utility): add enum extensions

---
+++ Utility/EnumExtensions.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Reflection;
+using TrueMogician.RimWorld.Utility.Attributes;
+using Verse;
+
+namespace TrueMogician.RimWorld.Utility;
+
+public static class EnumExtensions {
+	extension<T>(T self) where T : struct, Enum {
+		public string? Name => Enum.GetName(typeof(T), self);
+
+		public bool IsDefined => self.Name is not null;
+
+		public FieldInfo? FieldInfo => self.Name is { } name ? typeof(T).GetField(name) : null;
+
+		public string? Description => self.FieldInfo is not { } @field
+			? null
+			: @field.GetCustomAttribute<DescriptionAttribute>()?.description ??
+			@field.GetCustomAttribute<System.ComponentModel.DescriptionAttribute>()?.Description;
+
+		public string? Label => self.FieldInfo?.GetCustomAttribute<LabelAttribute>()?.Label;
+
+		public byte BitFlagCount {
+			get {
+				byte count = 0;
+				ulong value = Convert.ToUInt64(self);
+				while (value != 0) {
+					count += (byte)(value & 1);
+					value >>= 1;
+				}
+				return count;
+			}
+		}
+
+		public bool IsSingleBitFlag => self.BitFlagCount == 1;
+	}
+}
