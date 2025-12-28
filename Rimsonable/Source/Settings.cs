3193fcbca49f876be75f241a460f7f201e009d4b
true_mogician <truemogician@gmail.com>
2025-12-28 11:17:45 -0800

    refactor(Rimsonable): use new `FeatureSettings`

--- Rimsonable/Source/Settings.cs
+++ Rimsonable/Source/Settings.cs
@@ -4,20 +4,18 @@ using TrueMogician.RimWorld.Rimsonable.Patches;
 using TrueMogician.RimWorld.Rimsonable.Static;
 using TrueMogician.RimWorld.Utility;
 using TrueMogician.RimWorld.Utility.Attributes;
-using DescriptionAttribute = System.ComponentModel.DescriptionAttribute;
 
 namespace TrueMogician.RimWorld.Rimsonable;
 
 [Flags]
+[FeaturesEnum("Rimsonable.Settings.Features", true)]
 public enum Features : ulong {
 	None = 0,
 
-	[Label("Allow Grenades Through Shields")]
-	[Description("Allows grenades to pass through shield bubbles.")]
 	AllowGrenadesThroughShields = 1 << 0,
 }
 
-public class Settings() : FeatureSettings<Features>(Helper.Logger, "disabledFeatures") {
+public class Settings() : FeatureSettings<Features>(Helper.Logger) {
 	static Settings() {
 		AddFeaturePatches(Features.AllowGrenadesThroughShields, typeof(CompShieldPatches));
 	}
