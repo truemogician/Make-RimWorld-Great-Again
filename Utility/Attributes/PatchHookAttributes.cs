31593466af390cc416d5b1f680c891bb2c847030
true_mogician <truemogician@gmail.com>
2026-03-27 09:32:47 -0700

    feat(Utility): more robust parameter injection for patch hooks

--- Utility/Attributes/PatchHookAttributes.cs
+++ Utility/Attributes/PatchHookAttributes.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Reflection;
 using System.Runtime.CompilerServices;
 using HarmonyLib;
+using Verse;
 
 namespace TrueMogician.RimWorld.Utility.Attributes;
 
@@ -20,10 +21,12 @@ public enum PatchHookTiming : byte {
 }
 
 public static class PatchHookHelper {
+	private static readonly Type[] _hookParamTypes = [typeof(PatchHookTiming), typeof(Harmony)];
+
 	extension(Harmony harmony) {
 		public void PatchFromType(Type patchType) {
 			List<MethodInfo> beforeHooks = [], afterHooks = [];
-			foreach (var method in patchType.GetMethods(BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Static | BindingFlags.DeclaredOnly)) {
+			foreach (var method in patchType.GetMethods(AccessTools.allDeclared)) {
 				var attrs = method.GetCustomAttributes<PatchHookAttribute>().ToArray();
 				if (attrs.Length == 0)
 					continue;
@@ -34,15 +37,15 @@ public static class PatchHookHelper {
 					afterHooks.Add(method);
 			}
 			foreach (var beforeHook in beforeHooks)
-				InvokeHook(beforeHook, PatchHookTiming.BeforePatch);
+				InvokeHook(beforeHook, PatchHookTiming.BeforePatch, harmony);
 			harmony.CreateClassProcessor(patchType).Patch();
 			foreach (var afterHook in afterHooks)
-				InvokeHook(afterHook, PatchHookTiming.AfterPatch);
+				InvokeHook(afterHook, PatchHookTiming.AfterPatch, harmony);
 		}
 
 		public void UnpatchFromType(Type patchType) {
 			List<MethodInfo> beforeHooks = [], afterHooks = [];
-			foreach (var method in patchType.GetMethods(BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Static | BindingFlags.DeclaredOnly)) {
+			foreach (var method in patchType.GetMethods(AccessTools.allDeclared)) {
 				var attrs = method.GetCustomAttributes<PatchHookAttribute>().ToArray();
 				if (attrs.Length == 0)
 					continue;
@@ -53,23 +56,39 @@ public static class PatchHookHelper {
 					afterHooks.Add(method);
 			}
 			foreach (var beforeHook in beforeHooks)
-				InvokeHook(beforeHook, PatchHookTiming.BeforeUnpatch);
+				InvokeHook(beforeHook, PatchHookTiming.BeforeUnpatch, harmony);
 			harmony.CreateClassProcessor(patchType).Unpatch();
 			foreach (var afterHook in afterHooks)
-				InvokeHook(afterHook, PatchHookTiming.AfterUnpatch);
+				InvokeHook(afterHook, PatchHookTiming.AfterUnpatch, harmony);
 		}
 	}
 
 	[MethodImpl(MethodImplOptions.AggressiveInlining)]
-	private static void InvokeHook(MethodInfo method, PatchHookTiming timing) {
-		method.Invoke(null, method.GetParameters().Length == 0 ? null : [timing]);
+	private static void InvokeHook(MethodInfo method, PatchHookTiming timing, Harmony harmony) {
+		var @params = method.GetParameters();
+		if (@params.Length == 0)
+			method.Invoke(null, null);
+		else {
+			var values = new object[@params.Length];
+			var idx = @params.FirstIndexOf(p => p.ParameterType == typeof(PatchHookTiming));
+			if (idx != -1)
+				values[idx] = timing;
+			idx = @params.FirstIndexOf(p => p.ParameterType == typeof(Harmony));
+			if (idx != -1)
+				values[idx] = harmony;
+			if (values.Any(v => v == null))
+				throw new ArgumentException($"Could not resolve parameters for patch hook method {method.Name}.");
+			method.Invoke(null, values);
+		}
 	}
 
 	private static void ValidateHookMethod(MethodInfo method) {
 		if (!method.IsStatic)
 			throw new ArgumentException($"Patch hook method {method.Name} must be static.");
 		var @params = method.GetParameters();
-		if (@params.Length > 1 || (@params.Length == 1 && @params[0].ParameterType != typeof(PatchHookTiming)))
-			throw new ArgumentException($"Patch hook method {method.Name} must have zero or one parameter of type {nameof(PatchHookTiming)}.");
+		if (@params.Length > 2)
+			throw new ArgumentException($"Patch hook method {method.Name} cannot have more than two parameters.");
+		if (@params.Any(p => !_hookParamTypes.Contains(p.ParameterType)))
+			throw new ArgumentException($"Patch hook method {method.Name} has invalid parameter types. Allowed types are: {string.Join(", ", _hookParamTypes.Select(t => t.Name))}");
 	}
 }
