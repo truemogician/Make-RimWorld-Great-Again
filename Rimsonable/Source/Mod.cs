d2ce86786200e6b6149dfa3e0aebdb1f2e086e06
true_mogician <truemogician@gmail.com>
2026-03-28 15:38:49 -0700

    chore(Rimsonable): add Chinese title translation

--- Rimsonable/Source/Mod.cs
+++ Rimsonable/Source/Mod.cs
@@ -4,12 +4,14 @@ using Verse;
 namespace TrueMogician.RimWorld.Rimsonable;
 
 public class Mod : Verse.Mod {
+	private const string _TITLE_TRANSLATION_KEY = $"{nameof(Rimsonable)}.Title";
+
 	public Mod(ModContentPack content) : base(content) {
 		Settings.Default = GetSettings<Settings>();
 		LongEventHandler.QueueLongEvent(() => Settings.Default.Apply(), $"{ThisAssembly.Info.Title}-ApplySettings", true, null);
 	}
 
-	public override string SettingsCategory() => ThisAssembly.Info.Title;
+	public override string SettingsCategory() => _TITLE_TRANSLATION_KEY.TryTranslate(out var title) ? title : ThisAssembly.Info.Title;
 
 	public override void DoSettingsWindowContents(Rect inRect) => Settings.Default.DrawContents(inRect);
 }
