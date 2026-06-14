051ad8a79fa42167c67e4a3a520325b1eef13bf8
true_mogician <truemogician@gmail.com>
2026-06-14 01:32:24 -0700

    feat(BD): initialize new BattleDossier project

---
+++ BattleDossier/Source/Static/Helper.cs
@@ -0,0 +1,7 @@
+﻿using TrueMogician.RimWorld.Utility;
+
+namespace TrueMogician.RimWorld.BattleDossier.Static;
+
+public static class Helper {
+	public static readonly Logger Logger = new(ThisAssembly.Info.Title);
+}
